e464ba4e7796d8d6eebabed5be5a7a14bf753245
José Andrés Rodríguez <jarp820@gmail.com>
2019-07-29 15:32:07 -0400

    Updates

---
+++ precache-manifest.d9a17143ed2177b4fc6d898242ad210f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0dece0ad42942b0069f4cf85b8ea0d8",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "d4e32924cec9520d0938",
+    "url": "/personal-portfolio/static/css/main.b8524cc8.chunk.css"
+  },
+  {
+    "revision": "decf1ce3948d7d4aff0f",
+    "url": "/personal-portfolio/static/js/2.8d248648.chunk.js"
+  },
+  {
+    "revision": "d4e32924cec9520d0938",
+    "url": "/personal-portfolio/static/js/main.85c335a8.chunk.js"
+  },
+  {
+    "revision": "199df5cd20e1a679bb0f",
+    "url": "/personal-portfolio/static/js/runtime~main.8fd806ec.js"
+  },
+  {
+    "revision": "ab44e4f3616f64ad34c2539df5fdd8b4",
+    "url": "/personal-portfolio/static/media/about_me.ab44e4f3.jpg"
+  },
+  {
+    "revision": "331eb46e324d5ed71071804abe86284c",
+    "url": "/personal-portfolio/static/media/caracas-background-1.331eb46e.jpg"
+  },
+  {
+    "revision": "416250f60d785a2e02f17e054d2e4e44",
+    "url": "/personal-portfolio/static/media/flags.416250f6.png"
+  },
+  {
+    "revision": "d429a5777afaf2fc349652e812e9bb11",
+    "url": "/personal-portfolio/static/media/flags@2x.d429a577.png"
+  },
+  {
+    "revision": "19778cf9c33d0c98076aeffd7dd100aa",
+    "url": "/personal-portfolio/static/media/font-mfizz.19778cf9.ttf"
+  },
+  {
+    "revision": "4a873e5f62afe3c8a79b6250b3d477db",
+    "url": "/personal-portfolio/static/media/font-mfizz.4a873e5f.svg"
+  },
+  {
+    "revision": "4d03e427f13a893d5e9ce14ff5ed8287",
+    "url": "/personal-portfolio/static/media/font-mfizz.4d03e427.eot"
+  },
+  {
+    "revision": "64c7cf6cb98b1297cd522df93db3d0d2",
+    "url": "/personal-portfolio/static/media/font-mfizz.64c7cf6c.woff"
+  },
+  {
+    "revision": "298566aebf513b0c717da57d54d4d508",
+    "url": "/personal-portfolio/static/media/icomoon.298566ae.svg"
+  },
+  {
+    "revision": "7c65c6c2a044136f6814fbdb61b887c6",
+    "url": "/personal-portfolio/static/media/icomoon.7c65c6c2.ttf"
+  },
+  {
+    "revision": "fb32258d3b4ee1dff1b473cb45672c16",
+    "url": "/personal-portfolio/static/media/icomoon.fb32258d.eot"
+  },
+  {
+    "revision": "fdc5b3418777d81c9d170966d4dbda0f",
+    "url": "/personal-portfolio/static/media/icomoon.fdc5b341.woff"
+  },
+  {
+    "revision": "d77348fb508868c3c0ad99e2039a0d0c",
+    "url": "/personal-portfolio/static/media/laravel-ar21.d77348fb.svg"
+  },
+  {
+    "revision": "5c376133d6403088a87a04fcc9685674",
+    "url": "/personal-portfolio/static/media/nodejs-icon.5c376133.svg"
+  },
+  {
+    "revision": "96889989ac4b01f4ecba1ab719890d4b",
+    "url": "/personal-portfolio/static/media/postgresql-icon.96889989.svg"
+  },
+  {
+    "revision": "7038477259b6f1696308f867ce8c0a3a",
+    "url": "/personal-portfolio/static/media/reactjs-icon.70384772.svg"
+  },
+  {
+    "revision": "5d5fbf1437ee2844acaade02575b1fe4",
+    "url": "/personal-portfolio/static/media/sis-i-gif.5d5fbf14.gif"
+  }
+]);
